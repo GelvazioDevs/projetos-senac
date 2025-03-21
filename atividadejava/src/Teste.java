5c851f6268a628593f7cd8b0542881dd9a3db3e2
unknown <Senac@RSE011D056111.alunos.sc.senac.br>
2025-03-21 16:53:19 -0300

    ok

---
+++ atividadejava/src/Teste.java
@@ -0,0 +1,22 @@
+public class Teste {
+    // rodar programa com F5
+    // Atalhos 
+    // Criar método printipal
+    // psvm + TAB
+
+    public static void main(String[] args) {
+        // Atalho pra gerar mensagem
+        // sout + TAB
+                
+        System.out.println("Meu nome é Gelvazio Camargo.");
+        
+        // Atalhos do vscode
+        // Duplicar linha
+        // ALT + SHIFT + SETA PARA BAIXO        
+        System.out.println("Meu nome é Gelvazio Camargo.");
+
+        // Atalho para voltar 4 espaços ou 1 TAB
+        // SHIFT + TAB        
+    }
+}
+
---
+++ atividadejava/src/atividade21032025/Exercicio01.java
@@ -0,0 +1,26 @@
+package atividade21032025;
+
+// Crie uma sequência lógica para tomar banho.
+
+// Comentario de varias linhas
+/*
+ * Crie uma sequência lógica para tomar banho.
+ * Crie uma sequência lógica para tomar banho.
+ * Crie uma sequência lógica para tomar banho.
+ * Crie uma sequência lógica para tomar banho.
+ */
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        System.out.println("Dirigir-se ao chuveiro.");
+        System.out.println("Verificar se tem toalha para se secar.");
+        System.out.println("Verificar se tem roupa limpa para se vestir depois do banho.");
+        System.out.println("Verificar se tem água.");
+        System.out.println("Verificar se tem energia e se está esquentando a água.");
+        System.out.println("Entrar no box para tomar banho.");
+        System.out.println("Tomar banho.");
+        System.out.println("Sair do box.");
+        System.out.println("Se secar.");
+        System.out.println("Trocar de roupa.");
+    }
+}
