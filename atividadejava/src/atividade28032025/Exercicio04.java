3d37b3cdeab77216f30085ca536e016d54be312c
GelvazioDevs <gelvaziosenac2022@gmail.com>
2025-03-28 15:05:53 -0300

    ok

---
+++ atividadejava/src/atividade28032025/Exercicio04.java
@@ -0,0 +1,16 @@
+package atividade28032025;
+
+import java.util.Scanner;
+ 
+public class Exercicio04 {
+    
+    public static void main(String[] args) {        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um numero inteiro:\n ");
+        
+        int numero = entrada.nextInt();
+
+        System.out.println("inteiro lido: " + numero);
+    }
+}
