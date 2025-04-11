5506447d78ce40ca0ffd109c3f85ffb7bc909fe3
GelvazioDevs <gelvaziosenac2022@gmail.com>
2025-04-11 16:30:56 -0300

    ok

---
+++ atividadejava/src/atividade08042025/Exercicio03.java
@@ -0,0 +1,50 @@
+package atividade08042025;
+
+import java.util.Scanner;
+
+// Faca um algoritmo que leia o menor numero de tres digitados.
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("informe o numero n1:");
+        int n1 = entrada.nextInt();
+        
+        System.out.println("informe o numero n2:");
+        int n2 = entrada.nextInt();
+
+        System.out.println("informe o numero n3:");
+        int n3 = entrada.nextInt();
+
+        int menor = 0;
+        if(n1 < n2 && n1 < n3){
+            menor = n1;
+        }
+
+        if(n2 < n1 && n2 < n3){
+            menor = n2;
+        }
+
+        if(n3 < n1 && n3 < n2){
+            menor = n3;
+        }
+
+        // 3 SEQUENCIAS PARA O TESTE
+        // SEQUENCIA 01
+        // 1,2,3 -> menor = 1
+
+        // SEQUENCIA 02
+        // 3,2,1 -> menor = 1 - OK
+
+        // SEQUENCIA 03
+        // 2,1,3 -> menor = 1 - OK
+
+        // 3,1,2 -> menor = 1 - OK
+
+        // 2 ,3 ,1 - OK 
+
+        System.out.println("MENOR: " + menor);
+        entrada.close();
+    }
+}
---
+++ atividadejava/src/atividade08042025/Exercicio06.java
@@ -0,0 +1,65 @@
+package atividade08042025;
+
+import java.util.Scanner;
+
+// Faca um algoritmo que leia o maior, o menor e o intermediario de tres numeros digitados.
+public class Exercicio06 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("informe o numero n1:");
+        int n1 = entrada.nextInt();
+        
+        System.out.println("informe o numero n2:");
+        int n2 = entrada.nextInt();
+
+        System.out.println("informe o numero n3:");
+        int n3 = entrada.nextInt();
+
+        int maior = 0;
+        if(n1 > n2 && n1 > n3){
+            maior = n1;
+        }
+
+        if(n2 > n1 && n2 > n3){
+            maior = n2;
+        }
+
+        if(n3 > n1 && n3 > n2){
+            maior = n3;
+        }
+
+        int menor = 0;
+        if(n1 < n2 && n1 < n3){
+            menor = n1;
+        }
+
+        if(n2 < n1 && n2 < n3){
+            menor = n2;
+        }
+
+        if(n3 < n1 && n3 < n2){
+            menor = n3;
+        }
+
+        int intermediario = 0;
+        if(n1 < n2 && n1 > n3){
+            intermediario = n1;
+        }
+
+        if(n2 < n1 && n2 > n3){
+            intermediario = n2;
+        }
+
+        if(n3 < n1 && n3 > n2){
+            intermediario = n3;
+        }
+
+        System.out.println("MENOR: " + menor);
+        System.out.println("INTERMEDIARIO: " + intermediario);
+        System.out.println("MAIOR: " + maior);
+        
+        entrada.close();
+    }
+}
