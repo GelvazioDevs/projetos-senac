10e2d1a23a886ccac1d6bb80018dadd931ec6391
GelvazioDevs <gelvaziosenac2022@gmail.com>
2025-04-22 15:11:19 -0300

    ok

--- atividadejava/src/atividade22042025/Exercicio01.java
+++ atividadejava/src/atividade22042025/Exercicio01.java
@@ -1,5 +1,36 @@
 package atividade22042025;
 
+import java.util.ArrayList;
+
 public class Exercicio01 {
     
+    public static void main(String[] args) {
+        String aluno1 [] = {"Paulo", "22"};
+        String aluno2 [] = {"Ricardo", "74"};
+        String aluno3 [] = {"Thiago", "17"};
+        String aluno4 [] = {"Ellen", "44"};
+        String aluno5 [] = {"Julia", "32"};
+
+        ArrayList<String[]> listaDeAlunos = new ArrayList<String[]>();
+        listaDeAlunos.add (aluno1);
+        listaDeAlunos.add (aluno2);
+        listaDeAlunos.add (aluno3);
+        listaDeAlunos.add (aluno4);
+        listaDeAlunos.add (aluno5);
+
+        int alunosMaior30 = 0;
+        for (String[] aluno : listaDeAlunos){
+
+            String nome = aluno[0];
+            String idade = aluno[1];
+
+            int idadeint = Integer.parseInt(idade);
+            if (idadeint > 30) {
+                alunosMaior30++;
+            
+                
+            }
+        }
+        System.out.println("Alunos maiores que 30 anos:" + alunosMaior30);
+    }
 }
