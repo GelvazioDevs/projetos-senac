152056e695e78ff640638bb45c0bfc8f0bb2850a
GelvazioDevs <gelvaziosenac2022@gmail.com>
2025-05-06 14:05:32 -0300

    ok

---
+++ atividadejava/src/aula06052025/AnotacaoAula.java
@@ -0,0 +1,20 @@
+package aula06052025;
+
+public class AnotacaoAula {
+    
+    public static void main(String[] args) {
+        
+        // FUNCOES
+
+        // METODOS
+
+        // COM PARAMETRO
+
+        // SEM PARAMETROS
+
+        // CLASSES
+
+        // OBJETOS
+
+    }
+}
